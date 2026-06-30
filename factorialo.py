6bacb33968d32a98bb1e3f94956c46f7cdf80056
Prasheel Kr Singh <piyushsingh00bkp@gmail.com>
2026-06-30 20:18:23 +0530

    gonners

--- factorialo.py
+++ factorialo.py
@@ -1,5 +1,9 @@
 goon = int (input("Enter a number to find the factorial: "))
 fact =1 
-for i in range(1, goon + 1):
-    fact = fact * i
+# for i in range(1, goon + 1):
+#     fact = fact * i
+# print("The factorial of", goon, "is", fact) 
+while goon > 0:
+    fact = fact * goon
+    goon = goon - 1
 print("The factorial of", goon, "is", fact) 
