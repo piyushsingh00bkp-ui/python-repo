5c83813b61f9add1d37c2ea75775381afbda4ec2
Prasheel Kr Singh <piyushsingh00bkp@gmail.com>
2026-06-30 09:55:27 +0530

    some new loops

---
+++ weatheract.py
@@ -0,0 +1,9 @@
+weather = str(input("Enter the weather: "))
+if (weather == "sunny"):
+    print("It is a sunny day")
+elif (weather == "rainy"):
+    print("It is a rainy day")
+elif (weather == "cloudy"):
+    print("It is a cloudy day")
+else:
+    print("Invalid weather condition")
